cd8d5c722c71c2823408189010a913bcb8938da0
Aniuchi Adaobi Michelle <adamichelllle@gmail.com>
2019-05-17 12:40:59 +0100

    feature: Add indicator when hovering over columns

--- src/js/connect4game.js
+++ src/js/connect4game.js
@@ -5,21 +5,52 @@ class Connect4Game {
         this.gameBoardSelector = gameBoardSelector;
 
         this.createGameBoard();
+        this.addCoinToBoard();
     }
 
-    createGameBoard() {
+    createGameBoard = () => {
         const $gameBoard = $(this.gameBoardSelector);
 
         for( let row = 0; row < this.boardRows; row++) {
-            let $boardRow = $('<tr>').addClass('table-row');
+            let $boardRow = $('<tr>').addClass('board-row');
 
             for(let col = 0; col < this.boardCols; col++) {
-                let $boardCol = $('<td>').addClass('table-col table-col-empty');
+                let $boardCol = $('<td>')
+                .addClass('board-col empty')
+                .attr('data-board-col', col)
+                .attr('data-board-row', row);
+
                 $boardRow.append($boardCol);
             }
 
             $gameBoard.append($boardRow);  
         }
+    }
+
+    getLastEmptyCol = (colIndex) => {
+        const columns = $(`.board-col[data-board-col='${colIndex}']`);
+        for(let i = columns.length - 1; i >=0; i--) {
+            const $column = $(columns[i]);
+            if($column.hasClass('empty')) {
+                return $column;
+            }
+        }
+    }
+
+    addCoinToBoard = () => {
+        const $gameBoard = $(this.gameBoardSelector);
+        let that = this;
+
+        $gameBoard.on('mouseenter', '.board-col.empty', function() {
+            const colIndex = $(this).data('board-col');
+            const $lastEmptyColumn  = that.getLastEmptyCol(colIndex);
+            $lastEmptyColumn.addClass('next-yellow');
+
+        });
+
+        $gameBoard.on('mouseleave', '.board-col', function() {
+            $('.board-col').removeClass('next-yellow');
+        })
     }
 }
 
