daa85a03de83d8552df3dfe606917fdcb09c841d
Aniuchi Adaobi Michelle <adamichelllle@gmail.com>
2019-05-17 23:12:15 +0100

    Feature: Check for winner for all 4 possible outcomes
    Check if there's a winner vertically, horizontally, diagonally from top-left to bottom-right and diagonally from bottom-right to top-left

--- src/js/connect4game.js
+++ src/js/connect4game.js
@@ -1,22 +1,25 @@
 class Connect4Game {
     constructor(gameBoardSelector) {
-        this.boardRows = 6;
-        this.boardCols = 7;
-        this.gameBoardSelector = gameBoardSelector;
-        this.player = 'yellow';
+        this._gameBoardSelector = gameBoardSelector;
+        this._boardRows = 6;
+        this._boardCols = 7;
+        this._player = {
+            "player": "1",
+            "color": "yellow"
+        };
 
         this.createGameBoard();
         this.addCoinToBoard();
     }
 
     createGameBoard = () => {
-        const $gameBoard = $(this.gameBoardSelector);
+        const $gameBoard = $(this._gameBoardSelector);
 
-        for( let row = 0; row < this.boardRows; row++) {
+        for( let row = 0; row < this._boardRows; row++) {
             let $boardRow = $('<tr>').addClass('board-row');
 
-            for(let col = 0; col < this.boardCols; col++) {
-                let $boardCol = $('<td>')
+            for(let col = 0; col < this._boardCols; col++) {
+                let $boardCol = $('<td class="text-center">')
                 .addClass('board-col empty')
                 .attr('data-board-col', col)
                 .attr('data-board-row', row);
@@ -28,7 +31,46 @@ class Connect4Game {
         }
     }
 
-    getLastEmptyCol = (colIndex) => {
+    addCoinToBoard = () => {
+        const $gameBoard = $(this._gameBoardSelector);
+        let that = this;
+
+        $gameBoard.on('mouseenter', '.board-col.empty', function() {
+            const colIndex = $(this).data('board-col');
+            const $lastEmptyColumn  = that.getLastEmptySpot(colIndex);
+            $lastEmptyColumn.addClass(`next-${that._player.color}`);
+
+        });
+
+        $gameBoard.on('mouseleave', '.board-col', function() {
+            $('.board-col').removeClass(`next-${that._player.color}`);
+        });
+
+        $gameBoard.on('click', '.board-col.empty', function() {
+            const colIndex = $(this).data('board-col');
+
+            const $lastEmptySpot  = that.getLastEmptySpot(colIndex);
+            $lastEmptySpot.removeClass(`empty next-${that._player.color}`);
+            $lastEmptySpot.addClass(that._player.color);
+            $lastEmptySpot.data('player', that._player.player)
+        
+            const currentRowIndex = $lastEmptySpot.data('board-row');
+            const currentColIndex = $lastEmptySpot.data('board-col');
+
+            //check if there's a winner after each click
+            const winner = that.checkForWinner(currentRowIndex, currentColIndex);
+            if(winner) {
+                alert(`Player ${winner.player} has won`);
+                return;
+            }
+
+            that._player.player = (that._player.player === '1') ? '2' : '1';
+            that._player.color = (that._player.color === 'yellow') ? 'red': 'yellow';
+            $(this).trigger('mouseenter');
+        })
+    }
+
+    getLastEmptySpot = (colIndex) => {
         const columns = $(`.board-col[data-board-col='${colIndex}']`);
         for(let i = columns.length - 1; i >=0; i--) {
             const $column = $(columns[i]);
@@ -38,31 +80,62 @@ class Connect4Game {
         }
     }
 
-    addCoinToBoard = () => {
-        const $gameBoard = $(this.gameBoardSelector);
-        let that = this;
+    checkForWinner = (currentRowIndex, currentColIndex) => {
+        const directions = {
+            "vertically": {
+                "up": [-1, 0],
+                "down": [1, 0]
+            },
+            "horizontally": {
+                "left": [0, -1],
+                "right": [0, 1]
+            },
+            "diagonallyFromTLtoBR": {
+                "foward": [1, 1],
+                "backward": [-1, -1]
+            },
+            "diagonallyFromBRtoTL": {
+                "foward": [-1, 1],
+                "backward": [1, -1]
+            }
+        }
 
-        $gameBoard.on('mouseenter', '.board-col.empty', function() {
-            const colIndex = $(this).data('board-col');
-            const $lastEmptyColumn  = that.getLastEmptyCol(colIndex);
-            $lastEmptyColumn.addClass(`next-${that.player}`);
+        return (
+            this.checkPossibleWinOutcomes('vertically', directions.vertically.up, directions.vertically.down, currentRowIndex, currentColIndex) ||
+            this.checkPossibleWinOutcomes('horizontally', directions.horizontally.left, directions.horizontally.right, currentRowIndex, currentColIndex) ||
+            this.checkPossibleWinOutcomes('diagonallyFromTLtoBR', directions.diagonallyFromTLtoBR.foward, directions.diagonallyFromTLtoBR.backward, currentRowIndex, currentColIndex) ||
+            this.checkPossibleWinOutcomes('diagonallyFromBRtoTL', directions.diagonallyFromBRtoTL.foward, directions.diagonallyFromBRtoTL.backward, currentRowIndex, currentColIndex)
+        )
+    }
 
-        });
+    checkPossibleWinOutcomes = (outcome, direction1, direction2, cRowIndex, cColIndex) => {
+        const chainLength = 1 + this.checkDirection(direction1, cRowIndex, cColIndex) + this.checkDirection(direction2, cRowIndex, cColIndex);
 
-        $gameBoard.on('mouseleave', '.board-col', function() {
-            $('.board-col').removeClass(`next-${that.player}`);
-        });
+        if(chainLength >= 4) {
+            return this._player;
+        } else {
+            return null;
+        }
+    }
 
-        $gameBoard.on('click', '.board-col.empty', function() {
-            const colIndex = $(this).data('board-col');
+    checkDirection = (direction, cRowIndex, cColIndex) => {
+        let sum = 0;
+        let i = cRowIndex + direction[0];
+        let j = cColIndex + direction[1];
+        let $nextSimilarCoinSpot = this.getSimilarCoinSpot(i, j);
 
-            const $lastEmptyColumn  = that.getLastEmptyCol(colIndex);
-            $lastEmptyColumn.removeClass(`empty next-${that.player}`);
-            $lastEmptyColumn.addClass(that.player);
-            
-            that.player = (that.player === 'yellow') ? 'red' : 'yellow';
-            $(this).trigger('mouseenter')
-        })
+        while (i>=0 && i <this._boardRows && j>=0 && j<this._boardCols && 
+        $nextSimilarCoinSpot.data('player') === this._player.player) {
+            sum++;
+            i += direction[0];
+            j += direction[1];
+            $nextSimilarCoinSpot = this.getSimilarCoinSpot(i, j);
+        }
+        return sum;
+    }
+
+    getSimilarCoinSpot = (i, j) => {
+        return $(`.board-col[data-board-row='${i}'][data-board-col='${j}']`);
     }
 }
 
